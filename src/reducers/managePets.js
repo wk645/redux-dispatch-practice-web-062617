b638d1a167c2cee0e6e434c1efdcbff9f5d31773
wk645 <wk645@nyu.edu>
2017-09-14 10:05:29 -0400

    dispatch

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,26 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+	switch (action.type) {
+		case 'ADD_PET':
+			return {pets: state.pets.concat(action.pet)}
+			// Object.assign({}, state, {pets: [...state.pet, action.pet]})
+		case 'REMOVE_PET':
+			return ({...state, pets: state.pets.filter(pet => pet.id != action.id)})
+		default:
+			return state
+	}
 }
 
-export function dispatch(){
+export function dispatch(action){
+	state = managePets(state, action)
+	render()
 }
 
 export function render(){
+	const Lis = state.pets.map(pet => `<li>${pet.name}</li>`)
+
+	document.getElementById('container').innerHTML = `<ul>${Lis}</ul>`
 
 }
